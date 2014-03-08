b12851c5f84d16510a1034173df1f0af365f7497
Petr Vapenka <petr.vapenka@volny.cz>
2014-03-08 21:51:36 +0100

    Update project file

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject dumb-routing "0.0.2"
+  :url "https://github.com/bagl/clojure.dumb-routing"
+  :license  {:name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Dumb Ring compatible routing"
   :dependencies [[org.clojure/clojure "1.5.1"]])
