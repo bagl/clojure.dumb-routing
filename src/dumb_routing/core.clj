b4c2824eab333b5c263e27f35330198a4f2ba9eb
Petr Vapenka <petr.vapenka@volny.cz>
2014-03-08 20:12:47 +0100

    Use util namespace under alias (do not refer anything)

--- src/dumb_routing/core.clj
+++ src/dumb_routing/core.clj
@@ -1,12 +1,11 @@
 (ns dumb-routing.core
-  (:require [dumb-routing.util :refer [find-route
-                                       build-handler]]))
+  (:require [dumb-routing.util :as util]))
 
 (defn basic-handler [routes & [custom-not-found]]
   (fn [request]
     (let [uri (:uri request)
           not-found (or custom-not-found
                         (fn [_] {:status 404 :headers {} :body "not found"}))
-          route (find-route routes uri)
-          handler (if route (build-handler route uri) not-found)]
+          route (util/find-route routes uri)
+          handler (if route (util/build-handler route uri) not-found)]
         (handler request))))
